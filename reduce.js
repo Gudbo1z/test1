02c8f443ec511f2bf06ea9b52e9fe98d5e783f5f
Lê Anh <leanh@Les-MacBook-Air.local>
2022-09-08 13:06:49 +0700

    v2

--- reduce.js
+++ reduce.js
@@ -2,29 +2,18 @@ Array.prototype.reduceCus = function(callback, initialValue){
     let previous = this[0]
     let current = this[1]
     let length = this.length - 1
-    for(let i = 0; i < length; i++){
-        if(i == 0){ // trong lan chay dau tien 
-            //neu khong khoi tao init thi khong can thay doi arguments cua callback
-            if(initialValue){// neu co khoi tao init
-                previous = initialValue
-                current = this[0] // 0
-                length +=1
-            }
-            previous = callback(previous, current)
-        }
-        else{ // trong cac lan chay sau
-            // neu co init thi current = this[i], neu khong thi current = this[i+1]
-            current == this[0]? previous = callback(previous, this[i]) : previous = callback(previous, this[i+1])
-        }
+    if(initialValue){
+        previous = initialValue
+        current = this[0] // 0
+        length +=1
+    }
+    previous = callback(previous, current)
+    for(let i = 1; i < length; i++){
+        current == this[0]? previous = callback(previous, this[i]) : previous = callback(previous, this[i+1])
     }
     return previous
 }
-console.log([3,4,5].reduceCus((a,b)=>a+b))
 
+console.log([3,4,5].reduceCus((a,b)=>a+b))
 console.log([3,4,5].reduceCus((a,b)=>a+b,2))
 console.log([3,4,5].reduce((a,b)=>a+b,2))
-
-// if(current == this[0]){
-//     previous = callback(previous, this[i])
-// }
-// else previous = callback(previous, this[i])
