0ee33b5fc2cb71313e7fcf1f7d94b35d4927c8f1
Lê Anh <leanh@Les-MacBook-Air.local>
2022-09-08 14:45:47 +0700

    v3

--- reduce.js
+++ reduce.js
@@ -2,7 +2,7 @@ Array.prototype.reduceCus = function(callback, initialValue){
     let previous = this[0]
     let current = this[1]
     let length = this.length - 1
-    if(initialValue){
+    if(initialValue != undefined){
         previous = initialValue
         current = this[0] // 0
         length +=1
@@ -15,5 +15,5 @@ Array.prototype.reduceCus = function(callback, initialValue){
 }
 
 console.log([3,4,5].reduceCus((a,b)=>a+b))
-console.log([3,4,5].reduceCus((a,b)=>a+b,2))
-console.log([3,4,5].reduce((a,b)=>a+b,2))
+console.log([3,4,5].reduceCus((a,b)=>a+b,''))
+console.log([3,4,5].reduce((a,b)=>a*b,''))
