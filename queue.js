f48ef7983c7d18fb0cf04fc8c1680ba802e47679
Lê Anh <leanh@Les-MacBook-Air.local>
2022-09-13 11:02:16 +0700

    v2

--- queue.js
+++ queue.js
@@ -1,14 +1,19 @@
 class Queue {
     constructor (delay = 1000) {
         this.delay = delay
+        this.callbackQueue = [()=>console.log('xin chao1'), ()=>console.log('xin chao2'), ()=>console.log('xin chao3')]
+        this.status = false
     }
-    add(callback, callback1, callback2){
-        callback()
-        Object.values(arguments).forEach((argument, index) => {
-            if(index > 0)setTimeout(argument, this.delay*(index))
-        });
+    add(callback){
+        callback(); // callback se luon thuc hien
+        if(this.callbackQueue.length) { // neu callbackQueue ton tai
+            let cb = this.callbackQueue.splice(0,1)
+            console.log('-------')
+            setTimeout(() => this.add(cb[0]), this.delay)
+        }
     }
 }
-
 let test = new Queue
-test.add(()=>console.log('tao dung'), ()=>console.log('tao dung 1'), ()=>console.log('tao dung 2'))
+test.add(()=>console.log('xin chao'))
+
+// {setTimeout(()=>console.log('xin chao1'), 2000)}
