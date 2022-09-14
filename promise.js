29c4697c4f3d832898f5cb112174b3cac637c4ea
Lê Anh <leanh@Les-MacBook-Air.local>
2022-09-14 14:06:46 +0700

    v1

--- promise.js
+++ promise.js
@@ -3,22 +3,25 @@ const example = ['swift','rust','javascript','react','rx','ruby','rails','php','
 const show = document.querySelector('#show')
 
 function getApi(example){
-    let keywords = [...example]
-    if(keywords.length){
-        let apis = keywords.splice(0,5).map(keyword => fetch(`https://api.github.com/search/repositories?q=${keyword}`))
-        Promise.allSettled(apis)
-        .then(responses => {
-            responses.forEach((response, index) => {
-                if(response.status === 'fulfilled'){
-                    console.log(response.value.json().then(res => console.log(res.items[0])))
-                }
-                if(response.status === 'rejected'){
-                    keywords.push(keywords[index])
-                }
+    if(example.length){
+        let keywords = [...example]
+        if(keywords.length){
+            let apis = keywords.splice(0,5).map(keyword => fetch(`https://api.github.com/search/repositories?q=${keyword}`))
+            Promise.allSettled(apis)
+            .then(responses => {
+                responses.forEach((response, index) => {
+                    if(response.status === 'fulfilled'){
+                        show.innerHTML += (keywords[index] + ' ')
+                        console.log(response.value.json().then(res => console.log(res.items[0])))
+                    }
+                    if(response.status === 'rejected'){
+                        keywords.push(keywords[index])
+                    }
+                })
+                return keywords
             })
-            return keywords
-        })
-        .then(responses => getApi(responses))
+            .then(responses => getApi(responses))
+        }
     }
 }
 getApi(example)
