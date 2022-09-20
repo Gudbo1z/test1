e8ae80f157588e4954a57c10976d480b2c76049b
Lê Anh <leanh@Les-MacBook-Air.local>
2022-09-20 13:05:37 +0700

    v1

--- queue.js
+++ queue.js
@@ -1,29 +1,27 @@
 class Queue {
-    constructor(delay = 10000) {
+    constructor(delay = 1000) {
         this.delay = delay;
         this.callbackQueue = [];
+        this.current = 0
     }
 
     add(callback) {
-        if (this.callbackQueue.length === 0) { 
-            this.callbackQueue.push(callback);
-            return this.runcallback();
-        } else {
-            this.callbackQueue.push(callback);
-        }
+        this.callbackQueue.push(callback);
+        return this.runcallback()
     }
 
     runcallback() {
-        const callback = this.callbackQueue[0];
-        this.callbackQueue.shift();
-        if (this.callbackQueue.length > 0) {
-            let callback1 = this.callbackQueue[0]
-            return new Promise((res, rej) => {
+        const callback = this.callbackQueue[this.current];
+        if(this.callbackQueue.length > 1){
+            let callback1 = this.callbackQueue[this.current];
+            this.current +=1
+            return new Promise((res, rej)=>{
                 setTimeout(()=>{
                     res(callback1())
-                }, this.delay)
-            });
+                }, this.delay*this.current)
+            })
         }
+        this.current +=1
         return new Promise((res, rej) => res(callback()))
     }
 }
@@ -33,10 +31,19 @@ const cb2 = () => 2;
 const cb3 = () => 3;
 const cb4 = () => 4;
 
-const queue1 = new Queue(1000);
+const queue1 = new Queue(3000);
 let test1 = queue1.add(cb1);
 let test2 = queue1.add(cb2);
 let test3 = queue1.add(cb3);
-console.log(test1)
-console.log(test2)
-console.log(test3)
+let test4 = queue1.add(cb4);
+console.log(test1.then(res => console.log(new Date(), res)))
+console.log(test2.then(res => console.log(new Date(), res)))
+console.log(test3.then(res => console.log(new Date(), res)))
+console.log(test4.then(res => console.log(new Date(), res)))
+
+// const promise = new Promise((res, rej)=>{
+//     setTimeout(()=>{
+//         res('hoang anh')
+//     }, 2000)
+// })
+// promise.then(res => console.log('xin chao '+res))
